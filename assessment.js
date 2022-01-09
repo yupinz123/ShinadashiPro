78dd58997e0335f72a0fb8ec36e5d0f9d8426be0
yupinz123 <97378486+yupinz123@users.noreply.github.com>
2022-01-09 17:30:58 +0900

    あなたの使うべき原神キャラ診断

--- assessment.js
+++ assessment.js
@@ -27,12 +27,12 @@ assessmentButton.onclick = () => {
   const anchor = document.createElement('a');
   const hrefValue =
     'https://twitter.com/intent/tweet?button_hashtag=' +
-    encodeURIComponent('あなたのいいところ') +
+    encodeURIComponent('あなたの使うべき原神キャラ') +
     '&ref_src=twsrc%5Etfw';
   anchor.setAttribute('href', hrefValue);
   anchor.className = 'twitter-hashtag-button';
   anchor.setAttribute('data-text', result);
-  anchor.innerText = 'Tweet #あなたのいいところ';
+  anchor.innerText = 'Tweet #あなたの使うべき原神キャラ';
   tweetDivided.appendChild(anchor);
 
   // widgets.js の設定
@@ -48,22 +48,26 @@ assessmentButton.onclick = () => {
 };
 
 const answers = [
-  '{userName}のいいところは声です。{userName}の特徴的な声はみなを惹きつけ、心に残ります。',
-  '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
-  '{userName}のいいところは情熱です。{userName}の情熱に周りの人は感化されます。',
-  '{userName}のいいところは厳しさです。{userName}の厳しさがものごとをいつも成功に導きます。',
-  '{userName}のいいところは知識です。博識な{userName}を多くの人が頼りにしています。',
-  '{userName}のいいところはユニークさです。{userName}だけのその特徴が皆を楽しくさせます。',
-  '{userName}のいいところは用心深さです。{userName}の洞察に、多くの人が助けられます。',
-  '{userName}のいいところは見た目です。内側から溢れ出る{userName}の良さに皆が気を惹かれます。',
-  '{userName}のいいところは決断力です。{userName}がする決断にいつも助けられる人がいます。',
-  '{userName}のいいところは思いやりです。{userName}に気をかけてもらった多くの人が感謝しています。',
-  '{userName}のいいところは感受性です。{userName}が感じたことに皆が共感し、わかりあうことができます。',
-  '{userName}のいいところは節度です。強引すぎない{userName}の考えに皆が感謝しています。',
-  '{userName}のいいところは好奇心です。新しいことに向かっていく{userName}の心構えが多くの人に魅力的に映ります。',
-  '{userName}のいいところは気配りです。{userName}の配慮が多くの人を救っています。',
-  '{userName}のいいところはその全てです。ありのままの{userName}自身がいいところなのです。',
-  '{userName}のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる{userName}が皆から評価されています。'
+  '{userName}におすすめのキャラは甘雨です。重撃による瞬間火力を出せる甘雨は{userName}にとって非常に扱っていて楽しいキャラでしょう。',
+  '{userName}におすすめのキャラは胡桃です。{userName}にとって胡桃の重撃ループは非常に爽快でしょう。',
+  '{userName}におすすめのキャラは楓原万葉です。万葉の便利さは{userName}のチーム全体をサポートしてくれるでしょう。',
+  '{userName}におすすめのキャラは神里綾華です。使い勝手の良い神里綾華は{userName}にとって優秀なメインを張れるアタッカーとなるでしょう。',
+  '{userName}におすすめのキャラは雷電将軍です。{userName}が使う雷電将軍はチーム全員のサポーターとして非常に優秀なキャラとなるでしょう。',
+  '{userName}におすすめのキャラはウェンティです。ウェンティの元素爆発の強さは{userName}の日々の探索を楽にしてくれるでしょう。',
+  '{userName}におすすめのキャラは趙です。趙の落下攻撃は{userName}が行くどの秘境でも活躍する事でしょう。',
+  '{userName}におすすめのキャラは鍾離。{userName}にとって鍾離のシールドは必要不可欠な存在になるでしょう。',
+];
+
+var img = [];
+const images = [
+  img[0] = 'https://yupinz123.github.io/assessment/Ganyu.png',
+  img[1] = 'https://yupinz123.github.io/assessment/Hu Tao.png',
+  img[2] = 'https://yupinz123.github.io/assessment/Kaedehara Kazuha.png',
+  img[3] = 'https://yupinz123.github.io/assessment/Kamisato Ayaka.png',
+  img[4] = 'https://yupinz123.github.io/assessment/Raiden Shogun.png',
+  img[5] = 'https://yupinz123.github.io/assessment/Venti.png',
+  img[6] = 'https://yupinz123.github.io/assessment/Xiao.png',
+  img[7] = 'https://yupinz123.github.io/assessment/Zhongli.png',
 ];
 
 /**
@@ -83,7 +87,7 @@ function assessment(userName) {
   let result = answers[index];
 
   result = result.replaceAll('{userName}', userName);
-  return result;
+  return [result, index];;
 }
 
 // テストコード
